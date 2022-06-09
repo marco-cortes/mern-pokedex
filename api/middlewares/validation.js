739bf15dbd3e03efbb82f0cf9f0654641f067da9
Noe129 <noeramirez316@aragon.unam.mx>
2022-06-09 16:48:25 -0500

    [backend] added validation

---
+++ api/middlewares/validation.js
@@ -0,0 +1,18 @@
+const { response } = require("express");
+const { validationResult } = require("express-validator");
+
+const validation = (req, res = response, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
+
+    next();
+}
+
+module.exports = {
+    validation
+}
